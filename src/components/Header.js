216e884170dd3993fc6892dbd8b66cb17a99e428
Ignacio Sanz <37807147+Igsamu@users.noreply.github.com>
2024-05-26 14:08:14 +0200

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,9 @@
-import React, { useState, useEffect } from 'react';
+// src/components/Header.js
+import React from 'react';
 import './Header.css';
 import logo from '../media/Logo.webp';
-import sitioWeb from '../media/sitio-web.png';
-import usuario from '../media/usuario.png';
-import question from '../media/informacion.png';
-import net from '../media/sitio-web.png';
-import user from '../media/usuario.png';
-import { Arrow } from './Components.js';
 
-const Header = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 768);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsDesktop(window.innerWidth >= 768);
-    };
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
+const Header = ({ toggleMenu }) => {
   return (
     <header>
       <nav>
@@ -34,45 +12,6 @@ const Header = () => {
         </a>
         <button className="menu" onClick={toggleMenu}>Menú</button>
       </nav>
-      {isDesktop ? (
-        <div className="desktop-menu">
-          <div className="desk-but">
-            <button className="desktop-button">Vehículos</button>
-            <button className="desktop-button">Energía</button>
-            <button className="desktop-button">Carga</button>
-            <button className="desktop-button">Descubrir</button>
-            <button className="desktop-button">Tienda</button>
-          </div>
-          <div className="logos">
-            <button className="imgButton"><img src={question} alt='informacion' /></button>
-            <button className="imgButton"><img src={net} alt='web' /></button>
-            <button className="imgButton"><img src={user} alt='usuario' /></button>
-          </div>
-        </div>
-      ) : (
-        <div className={`menulistdiv ${menuOpen ? 'open' : ''}`} id="menuListDiv">
-          <button id="closeButton" onClick={toggleMenu}>X</button>
-          <div className="menuBut">
-            <button className="menuButton">Vehículos<span><Arrow /></span></button>
-            <button className="menuButton">Energía<span><Arrow /></span></button>
-            <button className="menuButton">Carga<span><Arrow /></span></button>
-            <button className="menuButton">Descubrir<span><Arrow /></span></button>
-            <button className="menuButton">Tienda</button>
-            <button className="menuButton">Soporte</button>
-            <button className="menuButton">
-              <span>
-                <img src={sitioWeb} alt="web" />España<br /><span className="subtext">español</span>
-              </span>
-              <span><Arrow /></span>
-            </button>
-            <button className="menuButton">
-              <span>
-                <img src={usuario} alt="usuario" />Cuenta
-              </span>
-            </button>
-          </div>
-        </div>
-      )}
     </header>
   );
 };
